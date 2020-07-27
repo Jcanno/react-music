f4f3c047504585be055a16657a4bcd1794f9cb2b
Jacano <641283231@qq.com>
2020-07-27 15:12:46 +0800

    feat:添加搜索

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
-import Home from '../application/Home';
 import Recommend from '../application/Recommend';
 import Singers from '../application/Singers';
 import Rank from '../application/Rank';
+import Home from '../application/Home';
 import Album from '../application/Album';
 import Singer from '../application/Singer';
+import Search from '../application/Search';
 
 export default [
   {
@@ -20,34 +21,47 @@ export default [
         )
       },
       {
-        path: "/recommend",
-				component: Recommend,
-				routes: [
-					{
-						path: "/recommend/:id",
-						component: Album
-					}
-				]
+        path: "/recommend/",
+        component: Recommend,
+        routes: [
+          {
+            path: "/recommend/:id",
+            component: Album
+          }
+        ]
       },
       {
         path: "/singers",
-				component: Singers,
-				routes: [
-					{
-						path: "/singers/:id",
-						component: Singer
-					}
-				]
+        component: Singers,
+        routes: [
+          {
+            path: '/singers/:id',
+            component: Singer
+          }
+        ]
       },
       {
-        path: "/rank",
-				component: Rank,
-				routes: [
-					{
-						path: "/rank/:id",
-						component: Album
-					}
-				]
+        path: "/rank/",
+        component: Rank,
+        key: "rank",
+        routes: [
+          {
+            path: "/rank/:id",
+            component: Album
+          }
+        ]
+      },
+      {
+        path: "/album/:id",
+        exact: true,
+        key: "album",
+        component: Album
+      },
+      {
+        path: "/search",
+        exact: true,
+        key: "search",
+        component: Search
       }
     ]
   }
